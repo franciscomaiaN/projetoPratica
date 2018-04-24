520936b8086117b397c150b725f3d1af5670cd16
franciscomaiaN <frn640@gmail.com>
2018-04-24 09:11:14 -0300

    adicionado o criar aula
    to na metade

---
+++ ProjetoPratica/ProjetoPratica/CriarMateria.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ProjetoPratica
+{
+    partial class CriarMateria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMateria = new System.Windows.Forms.TextBox();
+            this.txtAula = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnProxima = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.txtDisciplina = new System.Windows.Forms.TextBox();
+            this.cbxNumPer = new System.Windows.Forms.ComboBox();
+            this.cbxAlternativa = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAlternativa = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Disciplina:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Materia:";
+            // 
+            // txtMateria
+            // 
+            this.txtMateria.Location = new System.Drawing.Point(84, 51);
+            this.txtMateria.Name = "txtMateria";
+            this.txtMateria.Size = new System.Drawing.Size(121, 20);
+            this.txtMateria.TabIndex = 3;
+            // 
+            // txtAula
+            // 
+            this.txtAula.Location = new System.Drawing.Point(84, 89);
+            this.txtAula.Multiline = true;
+            this.txtAula.Name = "txtAula";
+            this.txtAula.Size = new System.Drawing.Size(256, 101);
+            this.txtAula.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Aulas:";
+            // 
+            // btnProxima
+            // 
+            this.btnProxima.Location = new System.Drawing.Point(265, 205);
+            this.btnProxima.Name = "btnProxima";
+            this.btnProxima.Size = new System.Drawing.Size(75, 23);
+            this.btnProxima.TabIndex = 6;
+            this.btnProxima.Text = "Proxima >>";
+            this.btnProxima.UseVisualStyleBackColor = true;
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(84, 205);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnAnterior.TabIndex = 7;
+            this.btnAnterior.Text = "<< Anterior";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Location = new System.Drawing.Point(376, 368);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(75, 23);
+            this.btnCriar.TabIndex = 8;
+            this.btnCriar.Text = "Criar";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(492, 41);
+            this.txtTitulo.Multiline = true;
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(256, 84);
+            this.txtTitulo.TabIndex = 10;
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(497, 273);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(32, 17);
+            this.rbA.TabIndex = 11;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(549, 273);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(32, 17);
+            this.rbB.TabIndex = 12;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(602, 272);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(32, 17);
+            this.rbC.TabIndex = 13;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(651, 273);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(33, 17);
+            this.rbD.TabIndex = 14;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // txtDisciplina
+            // 
+            this.txtDisciplina.Location = new System.Drawing.Point(84, 12);
+            this.txtDisciplina.Name = "txtDisciplina";
+            this.txtDisciplina.ReadOnly = true;
+            this.txtDisciplina.Size = new System.Drawing.Size(121, 20);
+            this.txtDisciplina.TabIndex = 15;
+            // 
+            // cbxNumPer
+            // 
+            this.cbxNumPer.FormattingEnabled = true;
+            this.cbxNumPer.Items.AddRange(new object[] {
+            "Pergunta 1",
+            "Pergunta 2",
+            "Pergunta 3",
+            "Pergunta 4",
+            "Pergunta 5"});
+            this.cbxNumPer.Location = new System.Drawing.Point(492, 9);
+            this.cbxNumPer.Name = "cbxNumPer";
+            this.cbxNumPer.Size = new System.Drawing.Size(121, 21);
+            this.cbxNumPer.TabIndex = 20;
+            // 
+            // cbxAlternativa
+            // 
+            this.cbxAlternativa.FormattingEnabled = true;
+            this.cbxAlternativa.Items.AddRange(new object[] {
+            "Alternativa A",
+            "Alternativa B",
+            "Alternativa C",
+            "Alternativa D"});
+            this.cbxAlternativa.Location = new System.Drawing.Point(492, 139);
+            this.cbxAlternativa.Name = "cbxAlternativa";
+            this.cbxAlternativa.Size = new System.Drawing.Size(121, 21);
+            this.cbxAlternativa.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(398, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Resposta certa:\r\n";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(397, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Texto Pergunta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Texto Alternativa:";
+            // 
+            // txtAlternativa
+            // 
+            this.txtAlternativa.Location = new System.Drawing.Point(492, 172);
+            this.txtAlternativa.Multiline = true;
+            this.txtAlternativa.Name = "txtAlternativa";
+            this.txtAlternativa.Size = new System.Drawing.Size(256, 84);
+            this.txtAlternativa.TabIndex = 25;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(640, 305);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "Adicionar Pergunta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // CriarMateria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 455);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAlternativa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbxAlternativa);
+            this.Controls.Add(this.cbxNumPer);
+            this.Controls.Add(this.txtDisciplina);
+            this.Controls.Add(this.rbD);
+            this.Controls.Add(this.rbC);
+            this.Controls.Add(this.rbB);
+            this.Controls.Add(this.rbA);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.btnCriar);
+            this.Controls.Add(this.btnAnterior);
+            this.Controls.Add(this.btnProxima);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAula);
+            this.Controls.Add(this.txtMateria);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CriarMateria";
+            this.Text = "CriarMateria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMateria;
+        private System.Windows.Forms.TextBox txtAula;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnProxima;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.TextBox txtDisciplina;
+        private System.Windows.Forms.ComboBox cbxNumPer;
+        private System.Windows.Forms.ComboBox cbxAlternativa;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAlternativa;
+        private System.Windows.Forms.Button button1;
+    }
+}
