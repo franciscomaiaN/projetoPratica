99b46c1cfde858582232c0ab5dff4abbaf56c7dd
franciscomaiaN <frn640@gmail.com>
2018-05-04 10:58:54 -0300

    ta indo

--- ProjetoPratica/ProjetoPratica/CriarMateria.cs
+++ ProjetoPratica/ProjetoPratica/CriarMateria.cs
@@ -12,9 +12,92 @@ namespace ProjetoPratica
 {
     public partial class CriarMateria : Form
     {
+        private string a = "";
+        private string b = "";
+        private string c = "";
+        private string d = "";
+        private string anterior = "A";
+
         public CriarMateria()
         {
             InitializeComponent();
+        }
+
+        private void cbxAlternativa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (anterior == "A")
+                a = txtAlternativa.Text;
+
+            if (anterior == "B")
+                b = txtAlternativa.Text;
+
+            if (anterior == "C")
+                c = txtAlternativa.Text;
+
+            if (anterior == "D")
+                d = txtAlternativa.Text;
+
+            anterior = cbxAlternativa.SelectedText;
+
+            if (anterior == "A")
+                txtAlternativa.Text = a;
+
+            if (anterior == "B")
+                txtAlternativa.Text = b;
+
+            if (anterior == "C")
+                txtAlternativa.Text = c;
+
+            if (anterior == "D")
+                txtAlternativa.Text = d;
+
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTitulo.Text))
+            {
+                MessageBox.Show("Voce deve preencher todos os campos!!");
+                return;
+            }
+
+            if (anterior == "A")
+                txtAlternativa.Text = a;
+
+            if (anterior == "B")
+                txtAlternativa.Text = b;
+
+            if (anterior == "C")
+                txtAlternativa.Text = c;
+
+            if (anterior == "D")
+                txtAlternativa.Text = d;
+
+
+
+            if (string.IsNullOrWhiteSpace(a))
+            {
+                MessageBox.Show("Voce deve preencher todas as alternativas!!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(b))
+            {
+                MessageBox.Show("Voce deve preencher todas as alternativas!!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(c))
+            {
+                MessageBox.Show("Voce deve preencher todas as alternativas!!");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(d))
+            {
+                MessageBox.Show("Voce deve preencher todas as alternativas!!");
+                return;
+            }
         }
     }
 }
